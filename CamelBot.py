d8259279b19a2083afc7f7a7834d73945b71d4f5
Christian Khoury <khourychristian@yahoo.com>
2024-07-01 16:12:30 -0700

    Logging bot into discord and confirming connection

--- CamelBot.py
+++ CamelBot.py
@@ -1,10 +1,32 @@
+import discord
 from discord.ext import commands
 
+bot = commands.Bot("/", intents=discord.Intents.all())
 
-def init_token():
-    with open("token.txt", "r") as file:
-        token = file.read()
-    return token
+
+class CamelBot:
+    def __init__(self, bot):
+        self.token = None
+        self.channel = None
+        self.bot = bot
+
+        self.init_token()
+
+        self.bot.run(self.token)
+    
+
+    def init_token(self):
+        with open("token.txt", "r") as file:
+            self.token = file.readline()
+            channel_id = int(file.readline())
+            self.channel = self.bot.get_channel(channel_id)
+
+
+@bot.event
+async def on_ready():
+    print("Hello World! This is Mr. Camel Bot")
+
+
 
 if __name__ == "__main__":
-    print(init_token())
+    CamelBot(bot)
