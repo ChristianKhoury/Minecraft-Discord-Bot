eeec7b070f08345758c860c7897da9bf1efafb30
Christian Khoury <khourychristian@yahoo.com>
2024-07-05 18:19:40 -0700

    Converting between nether and overworld coords

--- CamelBot.py
+++ CamelBot.py
@@ -9,10 +9,33 @@ with open("token.txt", "r") as file:
 
 @bot.event
 async def on_ready():
-    print("Hello World! This is Mr. Camel Bot")
+    # print("Hello World! This is Mr. Camel Bot")
     channel = bot.get_channel(channel_id)
-    await channel.send("Bot is alive!")
+    await channel.send("Restarting")
 
+
+@bot.command()
+async def OtoN(ctx, *args):
+    x = int(args[0]) / 8
+    if len(args) == 2:
+        y = int(args[1]) / 8
+        coords = str(x) + ", " + str(y)
+    else:
+        y = int(args[2]) / 8
+        coords = str(x) + ", " + args[1] + ", " + str(y)
+    await ctx.send("Nether Coords: " + coords)
+
+
+@bot.command()
+async def NtoO(ctx, *args):
+    x = int(args[0]) * 8
+    if len(args) == 2:
+        y = int(args[1]) * 8
+        coords = str(x) + ", " + str(y)
+    else:
+        y = int(args[2]) * 8
+        coords = str(x) + ", " + args[1] + ", " + str(y)
+    await ctx.send("Nether Coords: " + coords)
 
 if __name__ == "__main__":
     bot.run(token)
